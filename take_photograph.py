027c412a6b3bbad7f4c9f93424ad4b7e5eb2fa78
Tal Globus <talglobus@gmail.com>
2017-11-25 23:18:48 +0000

    Add debugging

--- take_photograph.py
+++ take_photograph.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
 import cv2, sys
+print(0)
 
 # Define constants
 DEVICE_NUMBER = 0
+print(1)
 
 # Initialize webcam
 vc = cv2.VideoCapture(DEVICE_NUMBER)
+print(2)
 
 # Check if the webcam works
 if vc.isOpened():
+	print(3)
 
 	# Try to get the first frame
 	retval, frame = vc.read()
+	print(4)
 else:
 
 	# Exit the program
+	print("Exiting")
 	sys.exit(1)
 # Read in the next frame
 
 retval, frame = vc.read()
+print(6)
 
 # Show the frame to the user
 cv2.imshow("DragonBoard 410c Workshop", frame)
+print(7)
 
 # Exit program after waiting indefinitely for a pressed key
 cv2.waitKey(0)
+print(8)
